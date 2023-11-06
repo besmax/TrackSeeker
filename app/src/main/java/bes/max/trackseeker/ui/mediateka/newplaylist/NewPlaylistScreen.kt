364932894b9cd08f126395ff7195fd40900e6353
Max <bespalov.m.9@ya.ru>
2023-11-06 11:29:33 +0300

    in player impl release was replaced by reset()

--- app/src/main/java/bes/max/trackseeker/ui/mediateka/newplaylist/NewPlaylistScreen.kt
+++ app/src/main/java/bes/max/trackseeker/ui/mediateka/newplaylist/NewPlaylistScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import bes.max.trackseeker.R
 import bes.max.trackseeker.domain.models.Track
+import bes.max.trackseeker.presentation.mediateka.newplaylist.NewPlaylistViewModel
 import bes.max.trackseeker.ui.theme.ysDisplayFamily
 import org.koin.androidx.compose.koinViewModel
 
